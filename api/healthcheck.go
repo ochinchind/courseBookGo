34a681ff3bc10e2391b27fd3c85caae99e94a43f
ochinchind <amanrahim93@gmail.com>
2023-09-26 01:01:36 +0600

    json healrg

--- api/healthcheck.go
+++ api/healthcheck.go
@@ -1,13 +1,28 @@
 package main
 
 
-import ( "fmt"
-
-"net/http"
+import ( 
+	"encoding/json"
+	"net/http"
 )
-// Declare a handler which writes a plain-text response with information about the // application status, operating environment and version.
+
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env) 
-	fmt.Fprintf(w, "version: %s\n", version)
-}
+	data := map[string]string{
+		"status": "available", 
+		"environment": app.config.env, 
+		"version": version,
+	}
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError) 
+		return
+	}
+
+	js = append(js, '\n')
+
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}	
