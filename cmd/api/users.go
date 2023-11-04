658411d0b303de03e42e8904f49483e7e9f9728e
ochinchind <amanrahim93@gmail.com>
2023-11-05 02:31:03 +0600

    send token to mail

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -5,6 +5,7 @@ import (
 	"coursego/internal/validator"
 	"errors"
 	"net/http"
+	"time"
 )
 
 func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -52,13 +53,19 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
 	if err != nil {
-		app.serverErrorResponse(w, r, err) 
+		app.serverErrorResponse(w, r, err)
+		return
 	}
 
 	app.background(func() {
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user) 
+		data := map[string]interface{}{
+			"activationToken": token.Plaintext,
+			"userID": user.ID, 
+		}
+
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", data)
 		if err != nil {
 			app.logger.PrintError(err, nil) 
 		}
