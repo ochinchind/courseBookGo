81384cb89bde89045fac75440e556bf4b986c22f
ochinchind <amanrahim93@gmail.com>
2023-11-05 01:45:41 +0600

    background tasks

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -34,6 +34,16 @@ func (app *application) serve() error {
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second) 
 		defer cancel()
 
+		err := srv.Shutdown(ctx) 
+		if err != nil {
+			shutdownError <- err 
+		}
+
+		app.logger.PrintInfo("completing background tasks", map[string]string{
+			"addr": srv.Addr, 
+		})
+
+		app.wg.Wait()
 		shutdownError <- srv.Shutdown(ctx)
 	}()
 
