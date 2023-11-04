bb516c074cadd6b55e2deb503f0286b96a35b476
ochinchind <amanrahim93@gmail.com>
2023-11-04 11:22:27 +0600

    server go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,8 +6,6 @@ import (
 	"coursego/internal/jsonlog"
 	"database/sql"
 	"flag"
-	"fmt"
-	"net/http"
 	"os"
 	"time"
 
@@ -26,6 +24,11 @@ type config struct {
 		maxIdleConns int 
 		maxIdleTime string
 	}
+	limiter struct {
+		rps float64 
+		burst int 
+		enabled bool
+	}
 }
 
 type application struct {
@@ -36,49 +39,33 @@ type application struct {
 
 func main() {
 	var cfg config
-
-
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
-	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)") 
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections") 
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections") 
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second") 
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst") 
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 	flag.Parse()
-
-
-
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
-	db, err := openDB(cfg)
- 
+	db, err := openDB(cfg) 
 	if err != nil {
-		logger.PrintFatal(err, nil)
+		logger.PrintFatal(err, nil) 
 	}
 	defer db.Close()
-
 	logger.PrintInfo("database connection pool established", nil)
-
-	app := &application{
+	app := &application{ 
 		config: cfg,
 		logger: logger,
-		models: data.NewModels(db),
+		models: data.NewModels(db), 
 	}
 
-	srv := &http.Server{
-		Addr: fmt.Sprintf(":%d", cfg.port),
-		Handler:  app.routes(),
-		IdleTimeout: time.Minute,
-		ReadTimeout: 10 * time.Second,
-		WriteTimeout: 30 * time.Second,
+	err = app.serve() 
+	if err != nil {
+		logger.PrintFatal(err, nil) 
 	}
-
-	logger.PrintInfo("starting server", map[string]string{
-		"addr": srv.Addr,
-		"env": cfg.env, 
-	})
-
-	err = srv.ListenAndServe()
-	logger.PrintFatal(err, nil)
 }
 
 func openDB(cfg config) (*sql.DB, error) {
