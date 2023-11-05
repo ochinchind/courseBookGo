02b81e3706c4510aef6d0b005586abf5ae8f65d1
ochinchind <amanrahim93@gmail.com>
2023-11-05 11:32:28 +0600

    checking if user is activated and authenticatied

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,11 +15,11 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler) 
 
 
-	router.HandlerFunc(http.MethodGet, "/v1/courses", app.listCoursesHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/courses", app.createCourseHandler) 
-	router.HandlerFunc(http.MethodGet, "/v1/courses/:id", app.showCourseHandler)
-	router.HandlerFunc(http.MethodPatch, "/v1/courses/:id", app.updateCourseHandler)
-	router.HandlerFunc(http.MethodDelete, "/v1/courses/:id", app.deleteCourseHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/courses", app.requireActivatedUser(app.listCoursesHandler))
+	router.HandlerFunc(http.MethodPost, "/v1/courses", app.requireActivatedUser(app.createCourseHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/courses/:id", app.requireActivatedUser(app.showCourseHandler))
+	router.HandlerFunc(http.MethodPatch, "/v1/courses/:id", app.requireActivatedUser(app.updateCourseHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/courses/:id", app.requireActivatedUser(app.deleteCourseHandler))
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
